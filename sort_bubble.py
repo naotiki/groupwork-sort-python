5d105b9173d5c003d76f1bb94d2d150dcd690d7c
Naotiki <naotiki@naotiki.me>
2025-01-25 14:45:33 +0900

    style: apply fmt

--- sort_bubble.py
+++ sort_bubble.py
@@ -9,12 +9,12 @@ class BubbleSort(SortMethod):
             # 隣接する要素を比較して交換
             for j in range(n - i - 1):
                 self.set("L", j)
-                self.set("R", j+1)
+                self.set("R", j + 1)
                 if self.target[j] > self.target[j + 1]:
                     # 交換
-                    #self.target[j], self.target[j + 1] = (
-                        #self.target[j + 1],
-                        #self.target[j],
-                    #)
-                    self.swap("L","R")
+                    # self.target[j], self.target[j + 1] = (
+                    # self.target[j + 1],
+                    # self.target[j],
+                    # )
+                    self.swap("L", "R")
         return self.target
